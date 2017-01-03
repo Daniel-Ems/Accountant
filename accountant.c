8b632eed2f38154b2958ea397f51e55535f78c8d
Daniel <danielems3@outlook.com>
2017-01-03 11:43:26 -0500

    At this stage, the program can accept paychecks and calculate them out over a month, quarter and year. It can also accept bills to a limited scope where it can calulate your profit only on a monthly basis. I must create additonal variables to hold the amount the make on different scales, year, month, week.

--- accountant.c
+++ accountant.c
@@ -11,24 +11,36 @@ int main()
 
 int flag = 0;
 char *buf = malloc(64 * sizeof(char));
-int cor;
+char *yesNo;
 
 while(flag == 0){
     printf("Please Tell me your name: ");
     scanf("%s", buf);
     char *name = buf;
+    printf("Debug Name: %s\n", name);
 
-    printf("Did you enter your name correctly? 0 = no, 1 = yes:");
-    scanf("%d", &cor);
-    flag = cor;   
+    printf("Did you enter your name correctly? yes or no?:");
+    scanf("%s", buf);
+    yesNo = lowercase(buf);
+    
+    if(strstr(yesNo, "n"))
+    {
+        flag = 0;  
+    }
+    else
+    {
+        flag = 1;
+    }
+       
     }
 
 puts(" ");
+
 printf("I am your accountant\nI will be helping you with your books.\n"
-        "Shall we begin? Yes or no?\n");
+        "Shall we begin? Yes or no?:");
+
 scanf("%s", buf);
 
-char *yesNo = buf;
 lowercase(yesNo);
 
 if (strstr(buf, "no"))
@@ -36,18 +48,46 @@ if (strstr(buf, "no"))
 printf("Okay, Goodbye.\n");
 exit(0);
 }
-
+puts(" ");
 float paycheck;
 printf("How much money do you make a paycheck?\n");
 
+int menu;
+
 scanf("%f", &paycheck);
-printf("for clarity, you make %.2f?\n", paycheck);
+printf("do you make %.2f\n(0)weekly\n(1)biweekly\n(2)monthly\n", paycheck);
+scanf("%d", &menu);
+
+if(menu == 0)
+{
+    printf("That means you bring home %.2f a month\n%.2f a quarter\n%.2f a year\n",
+             (paycheck * 4), (paycheck * 13), (paycheck * 52));
+}
+else if(menu == 1)
+{
+    printf("That means you bring home %.2f a month\n%.2f a quarter\n%.2f a year\n",
+             (paycheck * 2), ((paycheck/2)*13), ((paycheck/2) * 52));
+}
+else if(menu == 2)
+{
+     printf("That means you bring home %.2f biweekly\n%.2f a quarter\n%.2f a year\n",
+             (paycheck/2), ((paycheck/4)*13), ((paycheck/4) * 52));
+}
+
+puts(" ");
+
+float bills;
+printf("Okay, and how much do you pay in bills a month?:");
+scanf("%f", &bills);
+
+printf("That means you have %.2f of expendable cash a month\n %.2f a year\n",
+        ((paycheck * 2) - bills), (((paycheck*2) - bills) * 12));
 
 free(buf);
 }
 
 char *lowercase(char *yesNo){
-for(int a = 0; a < strlen(yesNo); a++)
+for(size_t a = 0; a < strlen(yesNo); a++)
 {
     yesNo[a]=tolower(yesNo[a]);
 }
